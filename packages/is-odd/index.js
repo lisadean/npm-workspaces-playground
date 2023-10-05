a44a2764bf5c5376c1d97c1874cb0aac4480e104
Lisa Dean <lisa.dean@supply.com>
2023-10-05 11:50:22 -0400

    stuff

---
+++ packages/is-odd/index.js
@@ -0,0 +1,2 @@
+import { isEven } from '@lisadean/is-even';
+export const isOdd = (n) => !isEven(n);
---
+++ packages/math-stuff/index.js
@@ -0,0 +1,7 @@
+import { isOdd } from '@lisadean/is-odd';
+import { isEven } from '@lisadean/is-even';
+
+console.log('Is 3 odd?', isOdd(3) ? 'Yes' : 'No');
+console.log('Is 3 even?', isEven(3) ? 'Yes' : 'No');
+console.log('Is 4 odd?', isOdd(4) ? 'Yes' : 'No');
+console.log('Is 4 even?', isEven(4) ? 'Yes' : 'No');
